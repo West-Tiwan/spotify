ebf7bd435c7e55f3dc050fbe16ed1267c258029a
West-Tiwan <xyzmohit.singh@gmail.com>
2023-08-19 20:46:15 +0530

    just need to add songs

---
+++ script.js
@@ -0,0 +1,13 @@
+class song_info {
+    constructor(name, producer, album, duration, source) {
+        this.song_name = name;
+        this.producer_name = producer;
+        this.length_of_song = duration;
+        this.sauce = source;
+    }
+}
+const i_have_no_enemies = song_info("Aruarian Dance","Nujabes","Stoic",170,music/I_have_no_enemies);
+const nier_1 = song_info("Voice of no return","Square Enix","Peacefull",231,music/nier_1);
+const nier_2 = song_info("Weight of the world","Square Enix","Peacefull",231,music/nier_2);
+const nier_3 = song_info("Sound of the end","Square Enix","Peacefull",231,music/nier_3);
+const emils_shop = song_info("emils_shop","Square Enix","Peacefull",231,music/emils_shop);
